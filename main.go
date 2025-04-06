c89cb87cfbadab56524e9ee3d0dfd6868ea3739d
Erik Knudsen <31932266+erik1066@users.noreply.github.com>
2025-04-06 10:12:01 -0400

    Updated file save function

--- main.go
+++ main.go
@@ -2,14 +2,54 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
+	"time"
 )
 
 func main() {
 	fmt.Println("Hello")
 }
 
+// Writes a new file and just deletes the old one (by renaming the new file to the old file)
+// If our update is interrupted e.g. by a crash, we still have gthe old file, and concurrent
+// readers won't get half-written data
 func SaveData(path string, data []byte) error {
+	tmp := fmt.Sprintf("%s.tmp.%d", path, randomInt())
+	fp, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0664)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		fp.Close()
+		if err != nil {
+			os.Remove(tmp)
+		}
+	}()
+
+	_, err = fp.Write(data)
+
+	if err != nil {
+		return err
+	}
+
+	err = fp.Sync()
+
+	if err != nil {
+		return err
+	}
+
+	// renaming a file replaces it atomically, and a delete is unnecessary
+	return os.Rename(tmp, path)
+}
+
+func SaveData_Old1(path string, data []byte) error {
+
+	/*
+		Problems: Must read all data into memory, change it, then write back; only usable for small data
+		Has concurrency problems.
+	*/
+
 	fp, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
 	if err != nil {
 		return err
@@ -23,4 +63,9 @@ func SaveData(path string, data []byte) error {
 	}
 
 	return fp.Sync()
+}
+
+func randomInt() int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Int()
 }
